97e7f20ea54d5cc783930c3926f76c281e03b3d5
Lawal Afeez <lawalafeez052@gmail.com>
2022-02-14 22:52:29 +0200

    add export contact functionality

--- Referral/settings.py
+++ Referral/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+# SECURITY WARNING: keep the secret key used in production secret
 SECRET_KEY = env("SECRET_KEY", default="unsafe-secret-key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
