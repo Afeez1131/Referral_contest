95366355f9ed7a494ef27a8543ce6c15d1626651
afeez1131 <lawalafeez052@gmail.com>
2022-09-20 12:31:01 +0100

    changes

--- Referral/settings.py
+++ Referral/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret
 SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "whatsapp-contest.herokuapp.com"]
 
